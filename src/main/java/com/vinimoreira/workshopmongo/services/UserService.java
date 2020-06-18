9775abf983635bb2d6736b01d3848be8194fa951
Vineee75 <viniciussouza81@yahoo.com.br>
2020-06-18 17:39:12 -0300

    Atualização do usuário com PUT

--- src/main/java/com/vinimoreira/workshopmongo/services/UserService.java
+++ src/main/java/com/vinimoreira/workshopmongo/services/UserService.java
@@ -13,20 +13,20 @@ import com.vinimoreira.workshopmongo.services.exceptions.ObjectNotFoundException
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repo;
 
 	// Métodos CRUD
-	
+
 	// C
-	
+
 	public User insert(User obj) {
 		return repo.insert(obj);
 	}
-	
+
 	// R
-	
+
 	public List<User> findAll() {
 		return repo.findAll();
 	}
@@ -35,21 +35,31 @@ public class UserService {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado!"));
 	}
-	
+
 	// U
-	
-	
+
+	public User update(User obj) {
+		User newObj = findById(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+
+	private void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
+	}
+
 	// D
-	
+
 	public void delete(String id) {
 		findById(id);
 		repo.deleteById(id);
 	}
-	
+
 	// Fim dos métodos CRUD
-	
+
 	public User fromDTO(UserDTO objDto) {
 		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
-	
+
 }

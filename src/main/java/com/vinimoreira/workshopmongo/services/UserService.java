14626fe2c5e07f50fb217e85a8f8701c13f48798
Vineee75 <viniciussouza81@yahoo.com.br>
2020-06-18 16:12:55 -0300

    Inserção de usuário com Post

--- src/main/java/com/vinimoreira/workshopmongo/services/UserService.java
+++ src/main/java/com/vinimoreira/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.vinimoreira.workshopmongo.domain.User;
+import com.vinimoreira.workshopmongo.dto.UserDTO;
 import com.vinimoreira.workshopmongo.repository.UserRepository;
 import com.vinimoreira.workshopmongo.services.exceptions.ObjectNotFoundException;
 
@@ -15,6 +16,16 @@ public class UserService {
 	
 	@Autowired
 	private UserRepository repo;
+
+	// Métodos CRUD
+	
+	// C
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	// R
 	
 	public List<User> findAll() {
 		return repo.findAll();
@@ -23,6 +34,17 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado!"));
+	}
+	
+	// U
+	
+	
+	// D
+	
+	// Fim dos métodos CRUD
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 	
 }
